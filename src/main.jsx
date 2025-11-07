ccaff6a2e5b990813a20b85f5ec9f3460dcf4db1
Enrique Herrera <comercial.integratextil@gmail.com>
2025-11-07 17:14:45 -0500

    Fix: avoid double rendering App; use RouterProvider without children to prevent DOM removal errors

--- src/main.jsx
+++ src/main.jsx
@@ -48,9 +48,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
 );
